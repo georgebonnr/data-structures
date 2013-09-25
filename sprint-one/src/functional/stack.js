163afb699909ef15091dc61c72e57104ee803919
Will Keleher <will.keleher@gmail.com>
2013-09-25 12:06:55 -0700

    stack is working with functional style

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,17 +3,26 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
 
   instance.push = function(value){
+    instance[size] = value;
+	size++;
   };
 
   instance.pop = function(){
+    if (size){
+	  size--;
+      return instance[size];
+    }
+    
+    	
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
