bb2718ed22947e52341d2fffae0eae69c265afb6
Will Keleher <will.keleher@gmail.com>
2013-09-25 14:42:54 -0700

    blah

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,6 +1,38 @@
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+  instance.size = 0;
+  instance.base = 0;
+  for (var key in queueMethods){
+    instance[key] = queueMethods[key];
+  }
+  return instance;
 };
 
-var queueMethods = {};
+var extend = function(to, from){
+  for (var key in from){
+    to[key] = from[key];
+  }
+};
+
+var queueMethods = {
+  enqueue: function(value){
+    console.log(this);
+    this[this.base + this.size] = value;
+    this.size++;
+  }, 
+
+  dequeue: function(){
+    if (this.size){
+	     this.size--;
+      var a = storage[base];
+      delete this[base];
+      this.base++;
+	     return a;
+	   }
+  },
+
+  size: function(){
+    return this.size;
+  } 
+};
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,25 +3,21 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size = 0;
-  var base = 0;
+  var size = 0;   //number of items in storage
+  var base = 0;   //location of the oldest item in the array
   // Implement the methods below
 
   instance.enqueue = function(value){
-    storage[base + size] = value;
+    storage[base + size] = value;   //base and value need to be summed so that it's adding to the end of the option
     size++;
-    console.log("base: " + base);
-    console.log("size: " + size);
   };
 
   instance.dequeue = function(){
     if (size){
 	     size--;
-      var a = storage[base];
+      var a = storage[base];    //var a is a temporary variable so that the item can be returned and the space cleared
       delete storage[base];
       base++;
-      console.log("base: " + base);
-      console.log("size: " + size);
 	     return a;
 	   }
   };
